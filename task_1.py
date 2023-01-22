16855a09144d4915595f8cf14913b56aa47089ba
VT <yapparov.han@mail.ru>
2023-01-22 19:03:14 +0500

    add change  task_1

--- task_1.py
+++ task_1.py
@@ -9,6 +9,97 @@
 
 #  Вариант человек против человека:
 
+# from random import randint
+
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"Ошибка! {name}, введите количество конфет от 1 до 28: "))
+#     return x
+
+
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# sweets = int(input("Введите количество конфет на столе: "))
+# flag = randint(0, 2)  # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0
+# counter2 = 0
+
+# while sweets> 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         sweets -= k
+#         flag = False
+#         p_print(player1, k, counter1, sweets)
+#     else:
+#         k = input_dat(player2)
+#         counter2 += k
+#         sweets -= k
+#         flag = True
+#         p_print(player2, k, counter2, sweets)
+
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+    
+    # ======================================================================================================
+    
+# Вариант человек против бота:
+# from random import randint
+
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"Ошибка! {name}, введите количество конфет от 1 до 28: "))
+#     return x
+
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = "Bot"
+# sweets = int(input("Введите количество конфет на столе: "))
+# flag = randint(0, 2)  # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0
+# counter2 = 0
+
+# while sweets > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         sweets -= k
+#         flag = False
+#         p_print(player1, k, counter1, sweets)
+#     else:
+#         k = randint(1, 29)
+#         counter2 += k
+#         sweets -= k
+#         flag = True
+#         p_print(player2, k, counter2, sweets)
+
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+    # ==========================================================================================================
+
+# Вариант человек против бота c "интеллектом":
 from random import randint
 
 def input_dat(name):
@@ -21,9 +112,15 @@ def input_dat(name):
 def p_print(name, k, counter, value):
     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
 
+def bot_calc(value):
+    k = randint(1, 29)
+    while value-k <= 28 and value > 29:
+        k = randint(1, 29)
+    return k
+
 
 player1 = input("Введите имя первого игрока: ")
-player2 = input("Введите имя второго игрока: ")
+player2 = "Bot"
 sweets = int(input("Введите количество конфет на столе: "))
 flag = randint(0, 2)  # флаг очередности
 if flag:
@@ -34,7 +131,7 @@ else:
 counter1 = 0
 counter2 = 0
 
-while sweets> 28:
+while sweets > 28:
     if flag:
         k = input_dat(player1)
         counter1 += k
@@ -42,7 +139,7 @@ while sweets> 28:
         flag = False
         p_print(player1, k, counter1, sweets)
     else:
-        k = input_dat(player2)
+        k = bot_calc(sweets)
         counter2 += k
         sweets -= k
         flag = True
